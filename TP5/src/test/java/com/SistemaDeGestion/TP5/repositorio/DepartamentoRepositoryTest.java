b0865b40d187c000c78e8b49bfafde940586c6da
IanOlmedo <i.olmedo@alumno.um.edu.ar>
2025-08-10 11:47:01 -0300

    Test Repositories

--- TP5/src/test/java/com/SistemaDeGestion/TP5/repositorio/DepartamentoRepositoryTest.java
+++ TP5/src/test/java/com/SistemaDeGestion/TP5/repositorio/DepartamentoRepositoryTest.java
@@ -25,5 +25,6 @@ class DepartamentoRepositoryTest {
 
         Optional<Departamento> hallado = departamentoRepository.findByNombre("RRHH");
         assertThat(hallado).isPresent();
+        
     }
 }
